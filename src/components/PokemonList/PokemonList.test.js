c9f70930141eddc566e974104c60b12bc8b981ce
yasin <yasintze@gmail.com>
2019-12-06 22:14:49 +0700

    update readme

--- src/components/PokemonList/PokemonList.test.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import React from "react";
-import { render } from "@testing-library/react";
-
-import PokemonList from "./PokemonList";
-
-it("renders pokemon list without error", () => {
-  render(<PokemonList />);
-});
